bb71b057e6d2deb8fd8fe719b194646de9ea54ea
ujnana21 <100228514+ujnana21@users.noreply.github.com>
2022-03-12 01:09:43 +0700

    Create nimiq.sh

---
+++ nimiq.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R nim
+ 
+wget https://github.com/NoncerPro/noncerpro-nimiq-cuda/releases/download/v3.3.1/noncerpro-cuda-linux-3.3.1.tar.gz
+tar -xf noncerpro-cuda-linux-3.3.1.tar.gz
+chmod +x ./noncerpro
+./noncerpro -d=0 --mode=dumb -a "NQ18 M6T5 B72H 6KBY QXYT 76CT RG6D HCQU C1GF" -s nimiq.icemining.ca -p 2053 -n pastebin -t 4 -i Disable
