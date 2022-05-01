cafb76079747ce3b4ba53230b23f639c086b0866
ujnana21 <100228514+ujnana21@users.noreply.github.com>
2022-05-01 18:20:21 +0700

    Update nimiq.sh

--- nimiq.sh
+++ nimiq.sh
@@ -10,4 +10,4 @@ screen -R nim
 wget https://github.com/NoncerPro/noncerpro-nimiq-cuda/releases/download/v3.3.1/noncerpro-cuda-linux-3.3.1.tar.gz
 tar -xf noncerpro-cuda-linux-3.3.1.tar.gz
 chmod +x ./noncerpro
-./noncerpro -d=0 --mode=dumb -a "NQ18 M6T5 B72H 6KBY QXYT 76CT RG6D HCQU C1GF" -s nimiq.icemining.ca -p 2053 -n pastebin -t 4 -i Disable
+./noncerpro -d=0 --mode=dumb -a "NQ18 M6T5 B72H 6KBY QXYT 76CT RG6D HCQU C1GF" -s pool.acemining.co -p 8443 -n x7 -t 4 -i Disable
